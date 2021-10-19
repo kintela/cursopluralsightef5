ba32d62d58ddd3ef158fe21a1d5c71c819d8204d
kintela <rquintela@fulcrum.es>
2021-10-19 18:29:49 +0200

    agregates

--- SamuraiApp/SamuariApp.UI/Program.cs
+++ SamuraiApp/SamuariApp.UI/Program.cs
@@ -12,12 +12,44 @@ namespace SamuariApp.UI
 
 		static void Main(string[] args)
 		{
-			_context.Database.EnsureCreated();
-			AddSamurais("Julie2", "Sampson2");
-			GetSamurais("After Add:");
+			//_context.Database.EnsureCreated();
+			//AddSamuraisByName("shimada", "Okamoto","Kikuchio","Hayashida");
+			//AddVariousTypes();
+			//GetSamurais("After Add:");
+			//QueryFilters();
+			QueryAggregates();
 			Console.WriteLine("Press any kay...");
 			Console.ReadKey();
 
+		}
+
+		private static void QueryAggregates()
+		{
+			var samurai = _context.Samurais.Find(2);
+			
+		}
+
+		private static void QueryFilters()
+		{
+			//var name = "Sampson";
+			//var samurais = _context.Samurais.Where(s => s.Name ==name).ToList();
+
+			var filter = "J%";
+			var samurais = _context.Samurais
+				.Where(s=>EF.Functions.Like(s.Name,filter)).ToList();
+			
+		}
+
+		private static void AddVariousTypes()
+		{
+
+			_context.AddRange(
+				new Samurai { Name = "Shimada" },
+				new Samurai { Name = "Okamoto" },
+				new Battle { Name = "Battle of Anegawa" },
+				new Battle { Name = "Battle of Nagashino" });
+
+			_context.SaveChanges();
 		}
 
 		private static void AddSamurai()
@@ -27,7 +59,7 @@ namespace SamuariApp.UI
 			_context.SaveChanges();
 		}
 
-		private static void AddSamurais(params string[] names)
+		private static void AddSamuraisByName(params string[] names)
 		{
 			foreach (string name in names)
 			{
